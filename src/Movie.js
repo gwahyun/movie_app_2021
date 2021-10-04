e87cb4b3a0a91c12f8b0103e259b5f779dfd5edf
gwahyun <bogosipeodochamah@gmail.com>
2021-10-04 21:23:20 +0900

    gh-page 준비

--- src/Movie.js
+++ src/Movie.js
@@ -15,7 +15,7 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <h3 className="movie__summary">{summary.slice(0, 180)}...</h3>
+        <h3 className="movie__summary">{summary.slice(0, 100)}...</h3>
       </div>
     </div>
   );
